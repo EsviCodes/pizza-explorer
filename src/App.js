d5739e874a20d41765d736ea3e4f87b207a74377
EsviCodes <esther.js.vis@gmail.com>
2019-10-03 14:56:26 +0200

    Shows Pizza Details on the page when you click on a pizza

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import PizzaListContainer from "./components/PizzaListContainer";
+import PizzaDetailsContainer from "./components/PizzaDetailsContainer";
 
 function App() {
   return (
     <div className="App">
       <PizzaListContainer />
+      <PizzaDetailsContainer />
     </div>
   );
 }
